721ad8d7c826deec094b27b575dd28cb84ef5d4d
Govind Gnanakumar <47906753+sandkoan@users.noreply.github.com>
2021-01-13 15:58:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from discord.ext import commands
 import chemlib
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# client = discord.Client()
 
 client = commands.Bot(command_prefix='-')
-with open('token.txt') as f:
-    token = f.readline() 
+
 
 @client.command(name='mass', description='Gets the molar mass of specified compound.')
 async def mass(ctx, cmpd):
@@ -11,4 +16,4 @@ async def mass(ctx, cmpd):
     except Exception as e: msg = str(e)
     await ctx.send(msg)
 
-client.run(str(token))
+client.run(TOKEN)
