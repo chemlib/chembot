5a4bd454bd3ff444b89b04ffeaf4eb637f5f731c
harirakul <harirakul.a@gmail.com>
2021-01-14 19:15:17 -0800

    Added the cmpd command

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 from discord.ext import commands
+import discord
 import chemlib
-import utils
+import argparse
 
-client = commands.Bot(command_prefix='#')
+client = commands.Bot(command_prefix='-')
 
 with open('token.txt') as f:
     token = f.readline() 
 
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('-cmpd', type=str)
+
+cmpd_parser = argparse.ArgumentParser(parents=[parser])
+cmpd_parser.add_argument('--amount', type=str)
+cmpd_parser.add_argument('--composition', type=str)
+
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(name="with substances"))
+    print(f'We have logged in as {client.user}')
 
-@client.command(name='mass', description='Gets the molar mass of specified compound.')
-async def mass(ctx, arg):
-    cmpd = chemlib.Compound(arg)
-    embed = utils.build_embed(
-        title = 'Molar Mass Calculator',
-        fields = [("Compound", arg), 
-                  ("Mass", f"{cmpd.molar_mass()} g/mol")
-                 ]
-    )
-    await ctx.send(embed = embed)
-
-@client.command(name='compound', description='Gets the general properties of a compound.')
-async def mass(ctx, arg):
-    cmpd = chemlib.Compound(arg)
-    embed = utils.build_embed(
-        title = f'{cmpd.formula} Properties',
-        fields = [("Mass", f"{cmpd.molar_mass()} g/mol")
-                 ]
-    )
+@client.command(name='cmpd', description='Gets the general properties of a compound.')
+async def cmpd(ctx, *args):
+    cmd = vars(cmpd_parser.parse_args(['-cmpd'] + list(args)))
+    compound = chemlib.Compound(cmd['cmpd'])
+    embed = discord.Embed(title=f'{compound.formula} Properties', color=0x7b2fde)
+    embed.add_field(name = "Molar Mass", value = str(compound.molar_mass()) + ' g/mol', inline=False)
+    
+    if cmd['amount'] is not None:
+        amt = cmd['amount']
+        if 'g' in amt: amts = compound.get_amounts(grams = float(amt[:-1]))
+        elif 'mol' in amt: amts = compound.get_amounts(moles = float(amt[:-3]))
+        else: amts = compound.get_amounts(molecules = float(amt[:-9]))
+        for field in amts:
+            if field not in ('compound', 'Compound'):
+                embed.add_field(name = field.capitalize(), value=amts[field], inline=True)
+    
+    if cmd['composition'] is not None:
+        elem = cmd['composition']
+        embed.add_field(name = f"% composition of {elem}", value = compound.percentage_by_mass(elem), inline=False)
+                
     await ctx.send(embed = embed)
 
 client.run(str(token))
